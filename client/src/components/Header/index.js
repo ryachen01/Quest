7fcc9929e6060c16ea8932a8dd2ecce1f34cd726
Your Name <ryachen@nuevaschool.org>
2019-04-25 17:51:48 -0700

    deleted unncessary functions in Header

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -79,63 +79,6 @@ class Header extends React.Component{
   };
 
 
-  getInput = async () => {
-
-
-  const {contract } = this.state;
-
-  var createAccount = window.confirm ("You have not created an account yet. Would you like to do so? ")
-
-  if (createAccount){
-
-  var username = prompt("Please enter an username: ", "e.g. joesmith1234");
-
-
-  const nameTaken = await contract.methods.isNameTaken(username).call();
-  if (username != null && !nameTaken) {
-      var name = prompt("Please enter a name: ", "Ryan");
-      if (name != null){
-        this.setState({ username: username});
-        this.setState({ name: name});
-
-        var x = window.confirm("Please Upload A Profile Photo");
-
-        if (x){
-        document.getElementById("profileUpload").click()
-
-        }
-
-
-    }
-  }
-  else{
-    this.getInput();
-  }
-}
-}
-createProfile = async () => {
-
-  const { accounts, contract, username, name, } = this.state;
-  var ipfsAPI = require('ipfs-api')
-
-  // connect to ipfs daemon API server
-  var ipfs = ipfsAPI('ipfs.infura.io', '5001', {protocol: 'https'})
-  // Connect to IPFS
-
-  const buf = Buffer.from(this.state.reader.result) // Convert data into buffer
-  ipfs.files.add(buf, (err, result) => { // Upload buffer to IPFS
-  if(err) {
-    console.error(err)
-    return false;
-  }
-
-  let hash = result[0].hash;
-
-  contract.methods.registerAccount(username, name, hash).send({from: accounts[0], value: 1e17, gasPrice: 1e9});
-
-})
-
-}
 openProfile = async () => {
 
   window.location.href = "/profile"
